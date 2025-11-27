4002971b01048db9d0d5d3d194a9462cd76ae4fa
mohdamaan9897 <khanamaan44696@gmail.com>
2025-11-27 14:47:52 +0530

    changes applied

---
+++ setup.py
@@ -0,0 +1,10 @@
+from setuptools import find_packages, setup
+
+setup(
+    name="medical-chatbot",
+    version="0.1.0",
+    author="Mohd Amaan",
+    author_email="khanamaan44696@gmail.com",
+    packages=find_packages(),
+    install_requires=[]
+)
