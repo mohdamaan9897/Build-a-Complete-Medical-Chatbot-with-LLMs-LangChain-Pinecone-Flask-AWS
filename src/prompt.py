7a55be4c0c8a852b2f68193d202736d255538a1a
mohdamaan9897 <khanamaan44696@gmail.com>
2025-11-27 21:56:21 +0530

    web app completed

---
+++ src/prompt.py
@@ -0,0 +1,8 @@
+system_prompt = (
+    "you are a helpful medical assistant."
+    " Use the context below to answer the question. "
+    " If you don't know the answer, just say that you don't know, don't try to make up an answer. use three sentences maximum and keep the"
+    " answer concise."
+    "\n\n"
+    "{context}"
+)
